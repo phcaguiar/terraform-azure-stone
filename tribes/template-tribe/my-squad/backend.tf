9cc9ab32f53cef220ba8b96db2b735db6110398d
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-11 00:22:36 +0000

    Add azure subscription for test

--- tribes/template-tribe/my-squad/backend.tf
+++ tribes/template-tribe/my-squad/backend.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "terraformazurestone"
     container_name       = "rg-tfstates"
     key                  = "my-squad-name.tfstate"
-    arm_subscription_id  = "my-azure-subscription-id"
+    arm_subscription_id  = "87d224fa-7526-478d-977c-d3b46e9aa5ce"
   }
 }
