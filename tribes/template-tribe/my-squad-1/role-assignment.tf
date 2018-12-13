e4f47e77ea77ec26cc52db8cf9d48e9b498380db
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-13 15:19:38 -0200

    add read permission to edu  assais in my-squad-1

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -2,11 +2,11 @@
 
 # Terraform resource to set development environment azure resource group permissions
 
-# resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-dev" {
-#   scope                = "${azurerm_resource_group.rg-dev.id}"
-#   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
-# }
+resource "azurerm_role_assignment" "assign-eduardo-assais-to-rg-my-squad-1-dev" {
+  scope                = "${azurerm_resource_group.rg-dev.id}"
+  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+  principal_id         = "129c90e0-7e14-4c4d-ae91-a3c527f7b951" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+}
 
 # Terraform resource to set staging environment azure resource group permissions
 
