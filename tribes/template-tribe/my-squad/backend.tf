7d893086cba534220099df6241ea182a346493ff
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-13 14:54:23 -0200

    change arm_subscription_id value

--- tribes/template-tribe/my-squad/backend.tf
+++ tribes/template-tribe/my-squad/backend.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "terraformazurestone"
     container_name       = "rg-tfstates"
     key                  = "my-squad-name.tfstate"
-    arm_subscription_id  = "87d224fa-7526-478d-977c-d3b46e9aa5ce"
+    arm_subscription_id  = "ba5141a2-2132-4e5a-b88d-d61f5a175fbe"
   }
 }
