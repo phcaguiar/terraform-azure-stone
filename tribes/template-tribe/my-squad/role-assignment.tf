f43a7fb55c6a901c6a7bb779d07bd8b2832b7127
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-13 17:12:33 +0000

    Addinf new test

--- tribes/template-tribe/my-squad/role-assignment.tf
+++ tribes/template-tribe/my-squad/role-assignment.tf
@@ -2,24 +2,24 @@
 
 # Terraform resource to set development environment azure resource group permissions
 
-resource "azurerm_role_assignment" "assign-eduardo-assais-to-rg-financial-dev" {
-  scope                = "${azurerm_resource_group.rg-dev.name}"
-  role_definition_name = "Reader"
-  principal_id         = "146709ad-f2ac-4df2-bfee-7d13cc5aee52"
-}
+# resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-dev" {
+#   scope                = "${azurerm_resource_group.rg-dev.id}"
+#   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+#   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+# }
 
 # Terraform resource to set staging environment azure resource group permissions
 
-# resource "azurerm_role_assignment" "user-name-stg" {
+# resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-stg" {
 #   scope                = "${azurerm_resource_group.rg-stg.id}"
-#   role_definition_name = ""
-#   principal_id         = ""
+#   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+#   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 # }
 
 # # Terraform resource to set production environment azure resource group permissions
 
-# resource "azurerm_role_assignment" "user-name-prd" {
+# resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-prd" {
 #   scope                = "${azurerm_resource_group.rg-prd.id}"
-#   role_definition_name = ""
-#   principal_id         = ""
+#   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+#   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 # }
