515b9fd3c883823814881df868485341c58fcc68
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-14 10:33:06 -0200

    change role permission

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -4,7 +4,7 @@
 
 resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-dev" {
   scope                = "${azurerm_resource_group.rg-dev.id}"
-  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+  role_definition_name = "Owner" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
   principal_id         = "146709ad-f2ac-4df2-bfee-7d13cc5aee52" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 }
 
