5d984b756877b630209bdded5f09dba8e8a25b1d
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-15 00:50:27 -0200

    remove last test permission

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -23,9 +23,3 @@
 #   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
 #   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 # }
-
-resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-dev" {
-  scope                = "${azurerm_resource_group.rg-dev.id}"
-  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-  principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
-}
