e23975676a861fae295311254b760fa8b9693597
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-15 02:18:49 -0200

    test change resource name

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -1,4 +1,4 @@
-resource "azurerm_role_assignment" "isouza-dev" {
+resource "azurerm_role_assignment" "abcde" {
   scope                = "${azurerm_resource_group.rg-dev.id}"
   role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
   principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
