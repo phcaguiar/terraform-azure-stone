7f365162af82a2c30b9194fa8809caa1e58c3f06
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-15 02:25:29 -0200

    run test dev and stg rg

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -10,6 +10,12 @@ resource "azurerm_role_assignment" "isouza-dev" {
 
 # Terraform resource to set staging environment azure resource group permissions
 
+resource "azurerm_role_assignment" "isouza-stg" {
+  scope                = "${azurerm_resource_group.rg-dev.id}"
+  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+  principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+}
+
 # resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-stg" {
 #   scope                = "${azurerm_resource_group.rg-stg.id}"
 #   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
