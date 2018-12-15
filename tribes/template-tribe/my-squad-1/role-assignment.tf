a88d228013b744ce6c25a7b315cf3828edc58601
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-15 02:47:47 -0200

    test

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -1,15 +1,15 @@
 resource "azurerm_role_assignment" "isouza-dev" {
   scope                = "${azurerm_resource_group.rg-dev.id}"
-  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-  principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+  role_definition_name = "Reader"
+  principal_id         = "isouza@stone.com.br"
 }
 resource "azurerm_role_assignment" "isouza-stg" {
   scope                = "${azurerm_resource_group.rg-stg.id}"
-  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-  principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+  role_definition_name = "Reader"
+  principal_id         = "isouza@stone.com.br"
 }
 resource "azurerm_role_assignment" "isouza-prd" {
   scope                = "${azurerm_resource_group.rg-prd.id}"
-  role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-  principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+  role_definition_name = "Reader"
+  principal_id         = "isouza@stone.com.br"
 }
