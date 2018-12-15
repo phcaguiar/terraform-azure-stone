f1cb898f73f8894d1e30de6a304e30f7467b8da8
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-15 02:16:57 -0200

    test only rg dev 2

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -1,25 +1,5 @@
-### Terraform scripts to set azure resource group permissions
-
-# Terraform resource to set development environment azure resource group permissions
-
 resource "azurerm_role_assignment" "isouza-dev" {
   scope                = "${azurerm_resource_group.rg-dev.id}"
   role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
   principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 }
-
-# Terraform resource to set staging environment azure resource group permissions
-
-# resource "azurerm_role_assignment" "isouza-stg" {
-#   scope                = "${azurerm_resource_group.rg-stg.id}"
-#   role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
-# }
-
-# # # Terraform resource to set production environment azure resource group permissions
-
-# resource "azurerm_role_assignment" "isouza-prd" {
-#   scope                = "${azurerm_resource_group.rg-prd.id}"
-#   role_definition_name = "Contributor" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
-# }
