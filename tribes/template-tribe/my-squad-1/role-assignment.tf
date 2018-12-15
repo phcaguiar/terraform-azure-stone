cf92805e36e2384ca8ea316f0fd8a7a4a4abc5a7
Pedro Aguiar <34889473+phcaguiar@users.noreply.github.com>
2018-12-15 02:11:18 -0200

    test

--- tribes/template-tribe/my-squad-1/role-assignment.tf
+++ tribes/template-tribe/my-squad-1/role-assignment.tf
@@ -2,36 +2,24 @@
 
 # Terraform resource to set development environment azure resource group permissions
 
-resource "azurerm_role_assignment" "isouza-dev" {
+resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-dev" {
   scope                = "${azurerm_resource_group.rg-dev.id}"
   role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
   principal_id         = "isouza@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 }
 
-# resource "azurerm_role_assignment" "edu-assais-dev" {
-#   scope                = "${azurerm_resource_group.rg-dev.id}"
-#   role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "eduardo.assais@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
-# }
+# Terraform resource to set staging environment azure resource group permissions
 
-# # Terraform resource to set staging environment azure resource group permissions
-
-# resource "azurerm_role_assignment" "dmorales-stg" {
+# resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-stg" {
 #   scope                = "${azurerm_resource_group.rg-stg.id}"
-#   role_definition_name = "Reader" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "dmorales@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+#   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+#   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 # }
 
 # # Terraform resource to set production environment azure resource group permissions
 
-# resource "azurerm_role_assignment" "cnatan-prd" {
+# resource "azurerm_role_assignment" "assign-user-name-to-rg-mytribe-prd" {
 #   scope                = "${azurerm_resource_group.rg-prd.id}"
-#   role_definition_name = "Contributor" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "cnatan@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
-# }
-
-# resource "azurerm_role_assignment" "dmorales-prd" {
-#   scope                = "${azurerm_resource_group.rg-prd.id}"
-#   role_definition_name = "Contributor" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
-#   principal_id         = "dmorales@stone.com.br" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
+#   role_definition_name = "" ## See in https://docs.microsoft.com/en-us/azure/role-based-access-control/
+#   principal_id         = "" ## Run az ad user show --upn-or-object-id user.mail@mail.com.br --query objectId
 # }
